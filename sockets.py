8c34dd7be4801adccfa2e7b091798cebfcfcb43b
Nevil Kandathil Sintho <nevil2207@gmail.com>
2023-03-30 02:40:04 -0600

    freetests passing, but not able to connect to socket from frontend/firefox not able to connect

--- sockets.py
+++ sockets.py
@@ -14,7 +14,7 @@
 # limitations under the License.
 #
 import flask
-from flask import Flask, request
+from flask import Flask, request, redirect
 from flask_sockets import Sockets
 import gevent
 from gevent import queue
@@ -63,26 +63,90 @@ myWorld = World()
 
 def set_listener( entity, data ):
     ''' do something with the update ! '''
+    send_all_json({entity: data})
+    
 
 myWorld.add_set_listener( set_listener )
-        
+
+
+# ---------------------------------------------------------------------------------------------
+
+# (c) 2014 Abram Hindle
+# Source: https://github.com/abramhindle/WebSocketsExamples/blob/master/broadcaster.py
+
+clients = list()
+
+# Updates each client
+def send_all(msg):
+    for client in clients:
+        client.put(msg)
+
+# Calls send_all to update all the clients
+def send_all_json(obj):
+    send_all(json.dumps(obj))
+
+class Client:
+    def __init__(self):
+        self.queue = queue.Queue()
+
+    def put(self, v):
+        self.queue.put_nowait(v)
+
+    def get(self):
+        return self.queue.get()
+
+# ---------------------------------------------------------------------------------------------
+
 @app.route('/')
 def hello():
     '''Return something coherent here.. perhaps redirect to /static/index.html '''
-    return None
+    return redirect("/static/index.html", code=302)
 
+# Used code from Abram Hindle
+# (c) 2014 Abram Hindle
+# Source: https://github.com/abramhindle/WebSocketsExamples/blob/master/broadcaster.py
 def read_ws(ws,client):
     '''A greenlet function that reads from the websocket and updates the world'''
     # XXX: TODO IMPLEMENT ME
-    return None
+    try:
+        while True:
+            msg = ws.receive()
+            print("WS RECV: %s" % msg)
+            if (msg is not None):
+                packet = json.loads(msg)
 
+                for e in packet:
+                    myWorld.set(e, packet[e])
+            else:
+                break
+    except Exception as e:
+        print("Error while receving %s" % e)
+
+# Again, used Hindle's code
+# (c) 2014 Abram Hindle
+# Source: https://github.com/abramhindle/WebSocketsExamples/blob/master/broadcaster.py
 @sockets.route('/subscribe')
 def subscribe_socket(ws):
     '''Fufill the websocket URL of /subscribe, every update notify the
        websocket and read updates from the websocket '''
     # XXX: TODO IMPLEMENT ME
-    return None
-
+    print("heladsad")
+    client = Client()
+    clients.append(client)
+    g = gevent.spawn( read_ws, ws, client )    
+    print("Subscribing")
+    try:
+        while True:
+            # block here
+            msg = client.get()
+            print("Got a message!")
+            ws.send(msg)
+    except Exception as e:# WebSocketError as e:
+        print("WS Error %s" % e)
+    finally:
+        clients.remove(client)
+        gevent.kill(g)
+    
 
 # I give this to you, this is how you get the raw body/data portion of a post in flask
 # this should come with flask but whatever, it's not my project.
@@ -99,25 +163,30 @@ def flask_post_json():
 @app.route("/entity/<entity>", methods=['POST','PUT'])
 def update(entity):
     '''update the entities via this interface'''
-    return None
+    # get json data
+    response = flask_post_json()
+
+    # print(response)
+    # update my world
+    myWorld.set(entity, response)
+    return json.dumps(response)
 
 @app.route("/world", methods=['POST','GET'])    
 def world():
     '''you should probably return the world here'''
-    return None
+    return json.dumps(myWorld.world())
 
 @app.route("/entity/<entity>")    
 def get_entity(entity):
     '''This is the GET version of the entity interface, return a representation of the entity'''
-    return None
-
+    return json.dumps(myWorld.get(entity))
 
 @app.route("/clear", methods=['POST','GET'])
 def clear():
     '''Clear the world out!'''
-    return None
-
-
+    myWorld.clear()
+    # redirects to main screen.
+    return redirect("static/index.html", code=200)
 
 if __name__ == "__main__":
     ''' This doesn't work well anymore:
